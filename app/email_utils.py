5a9381658fdef5aa3dc802417d20c7fba1c67d5e
Isabelle Netto <isabellenetto08@gmail.com>
2026-06-18 09:02:11 +0800

    Add Render Docker deployment

--- app/email_utils.py
+++ app/email_utils.py
@@ -1,26 +1,29 @@
+import os
 import smtplib
 from email.mime.text import MIMEText
 import streamlit as st
 
-EMAIL_ADDRESS = st.secrets["EMAIL_ADDRESS"]
-EMAIL_PASSWORD = st.secrets["EMAIL_PASSWORD"]
+
+def get_secret_value(key):
+    try:
+        return st.secrets[key]
+    except Exception:
+        return os.environ.get(key)
+
+
+EMAIL_ADDRESS = get_secret_value("EMAIL_ADDRESS")
+EMAIL_PASSWORD = get_secret_value("EMAIL_PASSWORD")
 
 
 def send_reset_email(to_email, code):
-
     subject = "Password Reset Code"
     body = f"Your password reset code is: {code}"
 
     msg = MIMEText(body)
-
     msg["Subject"] = subject
     msg["From"] = EMAIL_ADDRESS
     msg["To"] = to_email
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-        server.login(
-            EMAIL_ADDRESS,
-            EMAIL_PASSWORD
-        )
-
+        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         server.send_message(msg)
